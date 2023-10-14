bd87a2c8e8c0a9dc18a655ce17e889a06460660b
marcos-schlautmann <marcos27.bonfim@gmail.com>
2023-10-14 20:36:50 -0300

    Criando bootstrap.yml, add dependencias ao arquivo pox.xml

---
+++ src/main/java/br/com/marcosbonfim/SpringBootLocalstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosbonfim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLocalstackApplication.class, args);
+	}
+
+}
